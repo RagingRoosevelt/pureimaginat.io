2dfc4afd3ae96ca36da076e54cf72bb160b74113
Theodore S. Lindsey <theodore.lindsey@gmail.com>
2017-08-09 01:20:25 -0700

    Added fractal generator

---
+++ scripts/2DTest.js
@@ -0,0 +1,115 @@
+/**
+ * Created by Theodore on 3/8/2017.
+ */
+
+let canvas = undefined;
+
+$(function() {
+
+    canvas = document.getElementById("canvas");
+
+    window.onresize = function() {
+        console.log("window resized");
+        resizeCanvas();
+    };
+
+    resizeCanvas();
+       
+});
+
+function resizeCanvas() {$(canvas).css("height", "100vh");
+    $(canvas).css("width", "100vw");
+    
+    aspectRatio = ($(window).height() / $(window).width());
+    
+    canvas.width = 18;
+    canvas.height = canvas.width * aspectRatio;
+    
+    var viewRange;  
+    if (aspectRatio > (2/3)) {
+        viewRange = {xmin: -2, 
+                     xmax: 1, 
+                     ymin: - 0.5 * 3 * aspectRatio, 
+                     ymax: 0.5 * 3 * aspectRatio
+                     };
+    } else {
+        viewRange = {xmin: -(2/3) * 2 / aspectRatio, 
+                     xmax: (1/3) * 2 / aspectRatio, 
+                     ymin: -1, 
+                     ymax: 1
+                     };
+    }
+
+    ctx = canvas.getContext("2d");
+    
+    drawMandlebrot(ctx, 
+                   viewRange,
+                   2,
+                   10);
+           
+    /*
+    while (2 * canvas.width < $(window).width()) {
+        canvas.width *= 2;
+        canvas.height *= 2;
+        
+        drawMandlebrot(ctx, 
+                   viewRange,
+                   2,
+                   10);
+      
+        setTimeout(function() { canvas.width *= 2; canvas.height *= 2; drawMandlebrot(ctx, viewRange, radiusConvergence, maxIterations); }, 1000);
+    }
+    */
+}
+
+function newZ(z, c) {
+    return z.pow(2).add(c)
+}
+
+function drawMandlebrot(ctx, viewRange, radiusConvergence, maxIterations) {
+    console.log(canvas.width + "x" + canvas.height + " started (radius=" + radiusConvergence + ", maxIter=" + maxIterations + ")...");
+
+    var preCanvas = document.createElement('canvas');
+    preCanvas.width = canvas.width;
+    preCanvas.height = canvas.height;
+    var prectx = preCanvas.getContext('2d');
+    
+    var c, z, counter;
+    for (var cx = 0; cx < canvas.width; cx += 1) {
+        for (var cy = 0; cy < canvas.height; cy += 1) {
+            var x = (cx / canvas.width) * (viewRange.xmax - viewRange.xmin) + viewRange.xmin;
+            var y = (cy / canvas.height) * (viewRange.ymax - viewRange.ymin) + viewRange.ymin;
+            
+            c = new Complex([x,y]);
+            z = new Complex(0);
+            counter = 0;
+            while (z.abs() < radiusConvergence && counter < maxIterations) {
+                z = newZ(z,c);
+                counter += 1;
+            }
+            prectx.fillStyle = iterations2color(counter, maxIterations);
+            prectx.strokeStyle = iterations2color(counter, maxIterations);
+            prectx.fillRect( cx, cy, 1, 1);
+        }
+    }    
+        
+    ctx.putImageData(prectx.getImageData(0,0,preCanvas.width,preCanvas.height), 0, 0);
+    
+    //ctx.font = "16px Arial";
+    //ctx.fill = "#00000000"
+    //ctx.fillText(canvas.width + "x" + canvas.height,1,15);
+    
+    //ctx.renderAll();
+    
+    console.log("\t ... done");
+                   
+    if (canvas.width < $(window).width() && true) {
+        setTimeout(function() { canvas.width *= 2; canvas.height *= 2; drawMandlebrot(ctx, viewRange, radiusConvergence, maxIterations); }, 1000);
+    } else if (maxIterations < 100) {
+        setTimeout(function() { canvas.width *= 2; canvas.height *= 2; drawMandlebrot(ctx, viewRange, radiusConvergence, maxIterations + 5); }, 1000);
+    }
+}
+
+function iterations2color(iter, maxIter) {
+    return "hsl(0, 0%, " + 100*(maxIter-iter)/maxIter + "%)"
+}
--- scripts/master.js
+++ scripts/master.js
@@ -2,6 +2,69 @@
  * Created by Theodore on 3/3/2017.
  */
 
+let canvas = undefined;
+let gl = undefined;
+
 $(function() {
 
+    canvas = document.getElementById("canvas");
+
+    window.onresize = function() {
+        console.log("window resized");
+        resizeCanvas();
+    };
+
+
+    gl = initWebGL(canvas);
+
+    // Only continue if WebGL is available and working
+    if (!gl) {
+        return;
+    }
+
+    // Set clear color to black, fully opaque
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // Enable depth testing
+    gl.enable(gl.DEPTH_TEST);
+    // Near things obscure far things
+    gl.depthFunc(gl.LEQUAL);
+    // Clear the color as well as the depth buffer.
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+
+    resizeCanvas();
+
+
 });
+
+function initWebGL(canvas) {
+    gl = null;
+
+    // Try to grab the standard context. If it fails, fallback to experimental.
+    gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+
+    // If we don't have a GL context, give up now
+    if (!gl) {
+        alert('Unable to initialize WebGL. Your browser may not support it.');
+    }
+
+    return gl;
+}
+
+function resizeCanvas() {
+    if ($(window).height() > 1080 || $(window).width() > 1920) {
+        $(canvas).css("height", "100vh");
+        $(canvas).css("width", "100vw");
+        canvas.height = Math.round($(window).height() / 2);
+        canvas.width = Math.round($(window).width() / 2);
+    } else {
+        $(canvas).css("height", "");
+        $(canvas).css("width", "");
+        canvas.height = $(window).height();
+        canvas.width = $(window).width();
+    }
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+}
+
+
